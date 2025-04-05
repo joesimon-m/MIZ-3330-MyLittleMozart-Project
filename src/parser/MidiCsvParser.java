02c8ccddda09698d85ba5eeeac6a95e981195c57
gi6723 <giannicioannou@gmail.com>
2025-04-04 23:01:09 -0500

    I knew something was off with that csv file. Tore it apart using pandas determined malformations in the csv. Updated the parser to trim each field and remove surrounding quotes, modified the comparsion for the note event type to use Note_on_c and Note_off_c without a leading space and changed note on and off to use the correct MIDI constants.

--- src/parser/MidiCsvParser.java
+++ src/parser/MidiCsvParser.java
@@ -6,6 +6,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sound.midi.ShortMessage;
+
+/**
+ * Parses a CSV file and converts each line into MidiEventData object
+ */
 public class MidiCsvParser {
 	
 	/**
@@ -17,6 +22,7 @@ public class MidiCsvParser {
 	 * @throws IOException		throws an IOException when an error is caught
 	 */
 	public static List<MidiEventData> parseCsv(String filename) throws IOException{
+		
 		BufferedReader reader = null;
 		List<MidiEventData> lines = new ArrayList<MidiEventData>();
 		
@@ -26,8 +32,28 @@ public class MidiCsvParser {
 			
 			// reading through each line inside of the csv file
 			while((line = reader.readLine()) != null) {
-				// separates each value by a comma
+				
+				// Skipping any empty line
+				if(line.trim().isEmpty()) {
+					continue;
+				}
+				
+				//Seperating each value by the comma
 				String[] values = line.split(",");
+				
+				//Checking if the line has at least 6 values if not we skip it
+				if(values.length < 6) {
+					System.err.println("Skipping abnormal line (Not at least 6 values): " + line);
+					continue;
+				}
+				
+				//For each value we trim the whitespace and remove the surrounding quotes if they exsist
+				for (int i=0; i < values.length; i++) {
+					values[i] = values[i].trim();
+					if (values[i].startsWith("\"") && values[i].endsWith("\"")) {
+						values[i] = values[i].substring(1, values[i].length() - 1);
+					}
+				}
 				
 				int noteOnOff;
 				int startEndTick = Integer.parseInt(values[0]);
@@ -36,15 +62,20 @@ public class MidiCsvParser {
 				int velocity = Integer.parseInt(values[4]);
 				int instrument = Integer.parseInt(values[5]);
 				
-				if(values[1].equals(" Note_on_c")) {
-					noteOnOff = 1;
-				} else if (values[1].equals(" Note_off_c")) {
-					noteOnOff = 0;
+				// Comparing the event type against its expected values after trimming
+				if(values[1].equals("Note_on_c")) {
+					//noteOnOff = 1;
+					noteOnOff = ShortMessage.NOTE_ON;
+				} else if (values[1].equals("Note_off_c")) {
+					//noteOnOff = 0;
+					noteOnOff = ShortMessage.NOTE_OFF;
+
 				} else {
 					// throws an exception if noteOnOff is invalid (problem inside of csv file)
 					throw new IllegalArgumentException("Unrecognized noteOnOff value: " + values[1]);
 				}
 				
+				// Create a new MidiEventData object with the parsed values
 				MidiEventData midiLine = new MidiEventData(startEndTick, 
 						velocity, note, channel, instrument, noteOnOff);
 				
